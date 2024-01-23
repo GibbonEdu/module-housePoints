fe8eff55743a78f1da1a97cb3683b10575898771
Felix Olesen <felixolesen@gmail.com>
2024-01-23 16:22:30 +0800

    Changed award house points and Award student points forms to emphasise identical 'reason' fields. Added reason autocomplete dropdown for ease-of-use.

--- House Points/award.php
+++ House Points/award.php
@@ -59,11 +59,15 @@ if (isActionAccessible($guid, $connection2,"/modules/House Points/award.php")==F
             $row->addLabel('points', __('Points'));
             $row->addTextField('points')->disabled()->placeholder(__('Select a category'));
 
+        $reasons = $pdo->select("SELECT DISTINCT reason FROM hpPointHouse UNION SELECT DISTINCT reason from hpPointStudent ORDER BY reason")->fetchAll(\PDO::FETCH_COLUMN);
         $row = $form->addRow();
             $row->addLabel('reason', __('Reason'));
-            $row->addTextArea('reason')->setRows(2)->required();
-            
-         $row = $form->addRow();
+            $row->addTextField('reason')->maxLength(100)->required()->autocomplete($reasons);
+        
+        $row = $form->addRow();
+            $row->addAlert(__('If the reason for awarding points already exists, ensure the text in the reason field is the same.'), "warning");
+
+        $row = $form->addRow();
             $row->addFooter();
             $row->addSubmit();
 
